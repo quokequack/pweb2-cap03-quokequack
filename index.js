686659ecaff03c10a643c75b3554aebaea9498a4
quokequack <roymustangtaizo@gmail.com>
2026-08-18 20:22:48 -0300

    fix(rota-01): content-type re resposta modificados

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const server = http.createServer((req, res) => {
 
     if(req.method === "GET" && req.url === '/') {
         res.writeHead(200, {
-            "Content-Type": "text"
+            "Content-Type": "text/html"
         });
-        res.end(JSON.stringify({status: "Olá, Mundo!"}));
+        res.end("Olá, mundo!");
         return;
     }
 
