e4b929fe359f00b8527512201d220a934874b4c7
quokequack <roymustangtaizo@gmail.com>
2026-08-18 20:24:27 -0300

    feat(rota-02): responde GET /sobre com HTML e título Sobre

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ const server = http.createServer((req, res) => {
         return;
     }
 
+        if(req.method === "GET" && req.url === '/sobre') {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.end("<h1>Sobre</h1>");
+        return;
+    }
+    
+
 });
 
 server.listen(PORT, () => console.log(`Servidor em http://localhost:${PORT}`));
